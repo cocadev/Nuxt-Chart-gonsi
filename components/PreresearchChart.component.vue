246e65081a7f38147d505e25a5ee216be78255ef
cocadev <40970351+cocadev@users.noreply.github.com>
2022-05-31 16:16:41 +0800

    Initial commit

---
+++ components/PreresearchChart.component.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="PreresearchChartContent">
+    <apexcharts
+      class="chart"
+      type="radialBar"
+      width="180"
+      height="180"
+      :options="chartOptions"
+      :series="series"
+    />
+  </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+  name: 'PreresearchChart',
+  components: {
+    apexcharts: VueApexCharts
+  },
+  data () {
+    return {
+      series: [100, 95, 61, 90, 51],
+      chartOptions: {
+        chart: {
+          id: 'PreresearchChart'
+        },
+        plotOptions: {
+          radialBar: {
+            startAngle: 0,
+            endAngle: 270,
+            hollow: {
+              margin: 20,
+              size: '10%',
+              background: 'transparent'
+            },
+            track: {
+              background: '#044c80cc'
+            },
+            dataLabels: {
+              name: {
+                show: false
+              },
+              value: {
+                show: false
+              }
+            }
+          }
+        },
+        colors: ['#d59056', '#f8c84c', '#08e99b', '#02abff', '#015dff'],
+        legend: {
+          show: true,
+          floating: true,
+          position: 'left',
+          labels: {
+            useSeriesColors: true
+          },
+          markers: {
+            width: 4,
+            height: 4,
+            offsetX: -24,
+            offsetY: -12
+          },
+          itemMargin: {
+            vertical: -5.5,
+            horizontal: 100
+          },
+          formatter () {
+            return ''
+          }
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            legend: {
+              show: true
+            }
+          }
+        }]
+      }
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss">
+.PreresearchChartContent {
+  background: transparent;
+  position: relative;
+  .chart {
+    background: transparent;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    -moz-transform: scale(-1, 1);
+    -webkit-transform: scale(-1, 1);
+    -o-transform: scale(-1, 1);
+    -ms-transform: scale(-1, 1);
+    transform: scale(-1, 1);
+  }
+}
+</style>
---
+++ components/QualityChart.component.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="QualityChart">
+    <apexcharts
+      class="chart"
+      type="radialBar"
+      width="140"
+      height="140"
+      :options="chartOptions"
+      :series="series"
+    />
+  </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+  name: 'QualityChart',
+  components: {
+    apexcharts: VueApexCharts
+  },
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: '#000000'
+    },
+    percent: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      series: [70],
+      chartOptions: {
+        chart: {
+          id: 'QualityChart'
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              size: '60%'
+            },
+            track: {
+              background: '#044c80cc'
+            },
+            dataLabels: {
+              name: {
+                show: true,
+                color: '#ffffff'
+              },
+              value: {
+                show: false,
+                color: '#ff0000'
+              }
+            }
+          }
+        },
+        colors: ['#d59056'],
+        labels: ['']
+      }
+    }
+  },
+  created () {
+    this.chartOptions.labels = [this.label]
+    this.chartOptions.colors = [this.color]
+    this.series = [this.percent]
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss">
+.QualityChart {
+  background: transparent;
+  position: relative;
+}
+</style>
